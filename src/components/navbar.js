da8866a0ea59ffaeb96fb6e86f50f2d939208e37
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-01 19:14:21 -0800

    Added Navbar/components

---
+++ src/components/navbar.js
@@ -0,0 +1,29 @@
+//import {Nav, Form, FormControl, Button} from 'react-bootstrap';
+import React, { Component } from 'react';
+
+class Navbar extends Component{
+    render() {
+        return (
+            <>
+            {/* <Navbar bg="light" variant="light">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-primary">Search</Button>
+                </Form>
+            </Navbar> */}
+
+
+            </>
+        );
+
+    }
+    
+}
+
+export default Navbar; 
