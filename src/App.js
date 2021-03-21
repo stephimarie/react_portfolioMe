f9f7e680e633079bdbf14fafb7dca515ea06e4f8
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-21 16:16:01 -0700

    fix nav

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList} from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
-import Navbar from './components/navbar';
 
 class App extends Component {
   render() {
