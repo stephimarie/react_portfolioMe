255aebf1ed81d6a77cb09aff997a26a019be87f9
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-21 15:04:36 -0700

    Add yoda me card

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import Img1 from "../image/Yoda-Me-Astrology.png";
 import { Cell, Grid, Card, CardTitle, CardActions } from 'react-mdl';
 
 class Portfolio extends Component {
     render() {
         return (
-            <div className="portfolio-body">        
+            <div className="portfolio-body">  
+            <Grid className= "portfolio-grid">
+                <Cell col={3}>
+                    <Card shadow={0} style={{width: '320px', margin: 'auto', height: '320px'}}>
+                        <img src={Img1}/>
+                        <CardTitle style={{color: 'black', height: '100px'}}>Yoda-Me-Astrology</CardTitle>
+                    </Card>
+                </Cell>
+                </Grid>      
             </div>
-
-
 
         );
     };
