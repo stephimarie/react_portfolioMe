a499483f18278095bb9e6dd7b1fe7394a2a4d40a
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-09 05:49:37 -0700

    updated css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Navbar from '../src/components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content, Footer, FooterSection, FooterLinkList} from 'react-mdl';
-import Main from './components/main';
-import { Link } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="demo-big-content">
-        <Layout>
-          <Header className="header" title="SM" scroll>
-            <Navigation>
-              <Link to="/">Home</Link >
-              <Link to="/contact">Contact</Link >
-              <Link to="/portfolio">Portfolio</Link>
-
-            </Navigation>
-          </Header>
-          <Drawer title="Menu">
-            <Navigation>
-              <Link to="/">Home</Link>
-              <Link to="/contact">Contact</Link>
-              <Link to="/portfolio">Portfolio</Link>
-
-            </Navigation>
-          </Drawer>
-          <Content>
-              <div className="page-content" />
-              <Main/>
-          </Content>
-  <Footer size="mega">
-            <FooterSection type="middle">
-              <FooterLinkList>
-                <p>Stephanie Marie&copy;</p>
-              </FooterLinkList>
-            </FooterSection>
-          </Footer>
-        </Layout>
-      </div>
-    );
-  };
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>
+      
+    </>
+  );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom';
 
-
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </BrowserRouter>
+  , document.getElementById('root'));
 
-serviceWorker.unregister();
