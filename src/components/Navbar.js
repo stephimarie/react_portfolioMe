ec7bbde0b3abb9da33908aa7e6c5f3d2f3c91607
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-09 04:23:24 -0700

    icon added and hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { link }  from 'react-router-dom';
 
-function navbar() {
+function Navbar() {
+    const [click, setClick] = useState(false);
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
                     <link to="/" className="navbar-logo">
-                        SM <i className='fab fa-typo3'/>  
+                         SM <i class="fab fa-stripe-s"></i>
                     </link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                    </div>
                 </div>
             </nav>        
        </>
     )
 }
 
-export default navbar
+export default Navbar
