21ea91d263fc753824a0c65ea2d025a4355e5c43
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-09 05:36:21 -0700

    hide buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,25 @@ import { Button } from './Button.css'
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+    
     return (
         <>
             <nav className="navbar">
