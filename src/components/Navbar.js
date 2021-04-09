adee0e348ba2880a791fd4da19a1e7e18e615da8
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-09 03:48:47 -0700

    fixing navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <link to="/" className="navbar-logo">
+                        SM <i className='fab fa-typo3'/>  
+                    </link>
+                </div>
+            </nav>        
+       </>
+    )
+}
+
+export default navbar
