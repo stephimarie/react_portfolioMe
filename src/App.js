c4a2a66383ad93e1ee5ca85ac50d2f55d97a09ac
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-10 11:12:19 -0700

    set paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from '../src/components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/home';
+import About from './components/pages/About';
+import Portfolio  from './components/pages/Portfolio';
+
 
 function App() {
   return (
@@ -10,8 +13,9 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component=
-          {Home}/>
+          <Route path='/' exact component={Home}/>
+          <Route path='/about' exact component={About}/>
+          <Route path='/portfolio' exact component={Portfolio}/>
         </Switch>
       </Router>
       
