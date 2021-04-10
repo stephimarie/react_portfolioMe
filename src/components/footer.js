6d92a5fd93ada519944444d075910ad25acdadb2
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-10 11:47:54 -0700

    footer set up

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,38 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom';
-import Contact from './contact';
-import Home from './home';
-import Project from './project';
+import './Footer.css'
 
+function Footer() {
+    return (
+      <div className='footer-container'>
+        <section className='footer-subscription'>
+          <p className='footer-subscription-heading'>
+            Follow me and feel free to reach out.
+          </p>
+        </section>
 
-// const Footer = () => (
-//     <Footer size="mega">
-//     <FooterSection type="middle">
-//       <FooterLinkList>
-//         <p>Stephanie Marie &copy;</p>
-//       </FooterLinkList>
-//     </FooterSection>
-//   </Footer>
-// );
+        <div class='footer-links'>
+          <div className='footer-link-wrapper'>
+            <div class='footer-link-items'>
+              <h2>About Me</h2>
+              <Link to='/about'>About Me</Link>
+            </div>
+            <div class='footer-link-items'>
+              <h2>Contact Me</h2>
+              <Link to='/contact'>Contact</Link>
+            </div>
+          </div>
+          <div className='footer-link-wrapper'>
+            <div class='footer-link-items'>
+              <h2>Social Media</h2>
+              <Link to='/'>linkedin</Link>
+              <Link to='/'>Github</Link>
+            </div>
+          </div>
+        </div>
 
-// export default Footer;
+      </div>
+    );
+  }
+  
+  export default Footer;
+  
