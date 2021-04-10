f9fbb6963fa2389381b58427fade7a07b6187d95
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-10 13:34:26 -0700

    homesection add icons fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,24 +7,6 @@ function Footer() {
         <section class='social-media'>
           <div class='social-media-wrap'>
             <small class='website-rights'>Stephanie Marie © 2021</small>
-            <div class='social-icons'>
-              {/* <Link
-                class='social-icon-link Linkedin'
-                to= 'https://www.linkedin.com/in/stephanie-mccandless-76869139/'
-                target='_blank'
-                aria-label='Linkedin'
-              >
-                <i class="fab fa-linkedin"></i>
-              </Link>
-              <Link
-                class='social-icon-link github'
-                to='https://github.com/stephimarie'
-                target='_blank'
-                aria-label='GitHub'
-              >
-                <i class="fab fa-github"></i>
-              </Link> */}
-            </div>
           </div>
         </section>
       </div>
