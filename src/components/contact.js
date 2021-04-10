254125f3ddee833858c0338813dd08dc9c588faf
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-10 15:10:42 -0700

    About page

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,14 +9,15 @@ import Resume from '../images/resume.png'
 
 function Contact() {
     return (
-        <div className='home-container'>
+        <div className='contact-container'>
             <h1>Stephanie Marie</h1>
             <p>Full-Stack Developer</p>
             <p>Hello there, beautiful people.
-//             I am a full-stack developer, I have received my training through the UC Berkeley extension program. 
-//             I've been trained in HTML, CSS, Javascript, and node. If you give yourself no other option but
-//              to succeed then there will be no other possibility but success.</p>
+                I am a full-stack developer, I have received my training through the UC Berkeley extension program. 
+                If you give yourself no other option but
+                to succeed then there will be no other possibility but success.</p>
             <h5>HTML/CSS | Bootstrap | JavaScript | React </h5>
+            {/* may have to change name */}
                 <div className="social-media">
                     <a href="https://github.com/stephimarie" rel="noopener noreferrer" target="_blank">
                     <i class="fab fa-github" aria-hidden="true" />
