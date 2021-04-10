0fa455c56dafba5661b6f310bcb1a30b699c9164
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-10 12:54:53 -0700

    footer css fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import './Footer.css'
 
+        // <a href="https://github.com/stephimarie/Yoda_me_astrology" rel="noreferrer" target="_blank">Repo link</a>
+
+
+
 function Footer() {
     return (
       <div className='footer-container'>
         <section class='social-media'>
           <div class='social-media-wrap'>
-            <div class='footer-logo'>
-              <Link to='/' className='social-logo'>
-                Just S
-                <i class="fab fa-stripe-s"></i>
-              </Link>
-            </div>
             <small class='website-rights'>Stephanie Marie © 2021</small>
             <div class='social-icons'>
-              <Link
+              {/* <Link
                 class='social-icon-link Linkedin'
-                to='/https://www.linkedin.com/in/stephanie-mccandless-76869139/'
+                to= 'https://www.linkedin.com/in/stephanie-mccandless-76869139/'
                 target='_blank'
                 aria-label='Linkedin'
               >
@@ -24,12 +22,12 @@ function Footer() {
               </Link>
               <Link
                 class='social-icon-link github'
-                to='/https://github.com/stephimarie'
+                to='https://github.com/stephimarie'
                 target='_blank'
                 aria-label='GitHub'
               >
                 <i class="fab fa-github"></i>
-              </Link>
+              </Link> */}
             </div>
           </div>
         </section>
