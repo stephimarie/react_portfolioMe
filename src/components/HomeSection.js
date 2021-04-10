7aa8c64081a8c993bffab536d908a7ee6bc2a6c4
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-04-10 15:16:49 -0700

    home page

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -8,15 +8,6 @@ function homeSection() {
         <div className='home-container'>
             <h1>Stephanie Marie</h1>
             <p>Full-Stack Developer</p>
-            <h5>HTML/CSS | Bootstrap | JavaScript | React </h5>
-                <div className="social-media">
-                    <a href="https://github.com/stephimarie" rel="noopener noreferrer" target="_blank">
-                    <i class="fab fa-github" aria-hidden="true" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/stephanie-mccandless-76869139/" rel="noopener noreferrer" target="_blank">
-                    <i class="fab fa-linkedin" aria-hidden="true" />
-                    </a>
-                </div>
         </div>
     )
 }
